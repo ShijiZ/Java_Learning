fdb3b6ea901ed6ae4c9d97293a5032acdfb8b952
Shiji Zhao <zhaoshiji2016@gmail.com>
2019-06-05 11:51:30 -0700

    Java expressions & blocks session

---
+++ src/5Expressions_and_Blocks/JavaBlock.java
@@ -0,0 +1,11 @@
+public class JavaBlock {
+    public static void main(String[] args){
+
+        String band = "Beatles";
+
+        if (band == "Beatles"){ // Start of block
+            System.out.print("Hey ");
+            System.out.print("Jude!");
+        } // End of block
+    }
+}
