1383ccc549b425a0781fc03d48a5a3db3933c2d5
Shiji Zhao <zhaoshiji2016@gmail.com>
2019-06-05 12:05:30 -0700

    Java comment section

---
+++ src/6Comment/HelloWorld1.java
@@ -0,0 +1,11 @@
+/* This is a multi-line comment
+* The program prints "Hello World" to standard output
+* */
+
+class HelloWorld1 {
+    public static void main(String[] args){
+        {
+            System.out.println("Hello World!");
+        }
+    }
+}
---
+++ src/6Comment/HelloWorld2.java
@@ -0,0 +1,9 @@
+// Hello World Program
+
+public class HelloWorld2 {
+    public static void main(String[] args){
+        {
+            System.out.println("Hello, World!"); // prints "Hello World!"
+        }
+    }
+}
